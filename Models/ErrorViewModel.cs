387bcb92897129e4811dd356816314a4a891b203
fatih <fatihuyvar@hotmail.com>
2023-12-03 15:37:26 +0300

    Proje dosyası ekle.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebDevProje.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
